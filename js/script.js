a9f4806f1c72b5df5fa2458e831cce7c0619837c
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2022-02-22 14:24:03 -0300

    creat the scroll-suave

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import DropdownMenu from "./dropdown-menu.js";
+import ScrollSuave from "./scroll-suave.js";
 
 const dropdown = new DropdownMenu("[data-dropdown]", "active");
 dropdown.init();
+
+const scrollSuave = new ScrollSuave('[data-scroll="links"]');
+scrollSuave.init();
